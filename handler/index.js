bd5a80a1b145cbdd75cd693dd2aba99f4964685b
CeCeloz <haykkolocos@gmail.com>
2022-06-08 22:02:30 -0300

    Discord.js update permissions v2 slashcommands, right now this permissions is configured by discord owners in the tab inside the server
    Signed-off-by: CeCeloz <haykkolocos@gmail.com>

--- handler/index.js
+++ handler/index.js
@@ -1,12 +1,25 @@
 const {glob} = require("glob");
 const {promisify} = require("util");
 const {Client} = require("discord.js");
+
 const globPromise = promisify(glob);
 
 /**
  * @param {Client} client
  */
 module.exports = async (client) => {
+    // Commands
+    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+
+        if (file.name) {
+            const properties = {directory, ...file};
+            client.commands.set(file.name, properties);
+        }
+    });
 
     // Events
     const eventFiles = await globPromise(`${process.cwd()}/events/**/*.js`);
@@ -14,7 +27,7 @@ module.exports = async (client) => {
 
     // Slash Commands
     const slashCommands = await globPromise(
-        `${process.cwd()}/slashCommands/**/*.js`
+        `${process.cwd()}/SlashCommands/**/*.js`
     );
 
     const arrayOfSlashCommands = [];
@@ -31,39 +44,7 @@ module.exports = async (client) => {
     client.on("ready", async () => {
         const guild = client.guilds.cache.get("ID");
 
-        await guild.commands.set(arrayOfSlashCommands).then((cmd) => {
-            const getRoles = (commandName) => {
-                const permissions = arrayOfSlashCommands.find(x => x.name === commandName).userPermissions;
-                if (!permissions) return null;
-                return guild.roles.cache.filter((x) => x.permissions.has(permissions) && !x.managed
-                );
-            };
-
-            const fullPermissions = cmd.reduce((accumulator, x) => {
-                const roles = getRoles(x.name);
-                if (!roles) return accumulator;
-                const permissions = roles.reduce((a, v) => {
-                    return [
-                        ...a,
-                        {
-                            id: v.id,
-                            type: "ROLE",
-                            permission: true,
-                        },
-                    ];
-                }, []);
-                return [
-                    ...accumulator,
-                    {
-                        id: x.id,
-                        permissions,
-                    },
-                ];
-            }, []);
-
-            guild.commands.permissions.set({fullPermissions})
-        });
+        await guild.commands.set(arrayOfSlashCommands);
     });
-
 
 };
